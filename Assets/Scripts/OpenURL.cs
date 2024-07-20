7e476811cde1b6508011484c4a5c41b8c5c9cba0
brandon2500 <brandonflips@gmail.com>
2024-07-19 22:24:19 -0400

    Added twitter + discord buttons
    clicking button will take you to our twitter or discord

---
+++ Assets/Scripts/OpenURL.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenURL : MonoBehaviour
+{
+    public string twitterUrl = "https://www.x.com/BlitzBotzLinea";
+    public string discordUrl = "https://www.discord.com";
+
+
+    public void OpenTwitter()
+    {
+        Application.OpenURL(twitterUrl);
+    }
+
+    public void OpenDiscord()
+    {
+        Application.OpenURL(discordUrl);
+    }
+
+}
